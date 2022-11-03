f77292bf448e5ff506f506909fbf37262ba8fbb6
Andres Choque <49045402+andresChMx@users.noreply.github.com>
2022-11-03 04:10:59 -0500

    fix: swagger api documentation
    Se agrego la anotacion @EnableSwagger2 a la clase principal

--- src/main/java/com/appbient/restapi/RestapiApplication.java
+++ src/main/java/com/appbient/restapi/RestapiApplication.java
@@ -3,7 +3,10 @@ package com.appbient.restapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication()
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
 public class RestapiApplication {
 
 	public static void main(String[] args) {
